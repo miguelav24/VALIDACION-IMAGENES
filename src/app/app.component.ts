df324c55230db35c67ade7ebfbe01c2b1112032c
miguelav24 <miguel_rm_50@hotmail.com>
2019-03-24 18:14:51 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  uploadedFiles: any[] = [];
+  bannerDoc: string = "";
+  errorArray: Array<any> = [];
+
+  onSelect(event) {
+    for (let file of event.files) {
+      this.uploadedFiles.push(file);
+      var reader = new FileReader();
+
+      reader.onload = this._handleReaderLoaded.bind(this);
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  _handleReaderLoaded(e) {
+    let reader = e.target;
+    let imageSrc = reader.result;
+    
+    this.addImageProcess(imageSrc).then(img => {
+      let height = img['height'];
+      let width = img['width'];
+      let proporcion = width/height;
+
+      // Proporción estándar: 4 Margen: +-10%
+      if(proporcion < 3.6 || proporcion > 4.4) {
+        this.bannerDoc = "";
+        this.errorArray.push({severity:'info', summary:'Las dimensiones de la imagen no son válidas. Sólo están permitidas las que tengan una relación de aspecto cercana a 4:1', detail:'PrimeNG rocks'});
+      } else {
+        this.bannerDoc = imageSrc;
+        this.errorArray = [];
+      }
+    })    
+  }
+
+  addImageProcess(src){
+    return new Promise((resolve, reject) => {
+      let img = new Image()
+      img.onload = () => resolve(img)
+      img.onerror = reject
+      img.src = src
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FileUploadModule} from 'primeng/fileupload';
+import { HttpClientModule } from '@angular/common/http';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FileUploadModule,
+    HttpClientModule,
+    MessagesModule,
+    MessageModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
